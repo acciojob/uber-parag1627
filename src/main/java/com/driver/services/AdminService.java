80afa52f2fba2047e3351bd1ee52942a3a7258e9
parag1627 <paragpatil973047@gmail.com>
2023-03-18 21:28:46 +0530

    commit

--- src/main/java/com/driver/services/AdminService.java
+++ src/main/java/com/driver/services/AdminService.java
@@ -1,3 +1,24 @@
+//package com.driver.services;
+//
+//import java.util.List;
+//
+//import com.driver.model.Admin;
+//import com.driver.model.Customer;
+//import com.driver.model.Driver;
+//
+//public interface AdminService {
+//
+//	public void adminRegister(Admin admin);
+//
+//	public Admin updatePassword(Integer adminId, String password);
+//
+//	public void deleteAdmin(int adminId);
+//
+//	public List<Driver> getListOfDrivers();
+//
+//	public List<Customer> getListOfCustomers();
+//}
+
 package com.driver.services;
 
 import java.util.List;
@@ -15,6 +36,7 @@ public interface AdminService {
 	public void deleteAdmin(int adminId);
 
 	public List<Driver> getListOfDrivers();
-	
+
 	public List<Customer> getListOfCustomers();
 }
+
--- src/main/java/com/driver/services/CustomerService.java
+++ src/main/java/com/driver/services/CustomerService.java
@@ -1,3 +1,28 @@
+//package com.driver.services;
+//
+//
+//import java.util.List;
+//
+//import com.driver.model.Customer;
+//import com.driver.model.Driver;
+//import com.driver.model.TripBooking;
+//
+//
+//public interface CustomerService {
+//
+//	public void register(Customer customer);
+//
+//	public void deleteCustomer(Integer customerId);
+//
+//	public TripBooking bookTrip(int customerId, String fromLocation, String toLocation, int distanceInKm) throws Exception;
+//
+//	public void cancelTrip(Integer tripId);
+//
+//	public void completeTrip(Integer tripId);
+//
+//}
+
+
 package com.driver.services;
 
 
@@ -13,11 +38,11 @@ public interface CustomerService {
 	public void register(Customer customer);
 
 	public void deleteCustomer(Integer customerId);
-	
+
 	public TripBooking bookTrip(int customerId, String fromLocation, String toLocation, int distanceInKm) throws Exception;
-	
+
 	public void cancelTrip(Integer tripId);
 
 	public void completeTrip(Integer tripId);
-	
+
 }
